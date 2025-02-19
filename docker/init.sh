12d793382ffb4e429301ae1bce121db77e71280b
asergio-ti <asergio.ti@gmail.com>
2025-02-19 14:48:03 -0300

    feat: configura ambiente para Codespaces com SSH nativo

--- docker/init.sh
+++ docker/init.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 set -e
 
-# Configurar permissões SSH
-if [ -d "/home/node/.ssh" ]; then
-    chmod 700 /home/node/.ssh
-    if [ -f "/home/node/.ssh/authorized_keys" ]; then
-        chmod 600 /home/node/.ssh/authorized_keys
-    fi
-fi
-
-# Iniciar o servidor SSH em background
-sudo service ssh start
-
 # Se existir um package.json no diretório client, instalar dependências
 if [ -f "client/package.json" ]; then
     echo "Installing client dependencies..."

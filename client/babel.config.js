1651f44153239f329df09999c29c57b543b298a5
asergio-ti <asergio.ti@gmail.com>
2025-02-20 00:22:59 -0300

    fix: ajusta configuração do Babel para CommonJS

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,7 +1,10 @@
-module.exports = {
+/** @type {import('@babel/core').TransformOptions} */
+const config = {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript',
     ['@babel/preset-react', { runtime: 'automatic' }]
   ]
-}; 
+};
+
+module.exports = config; 
